5ddbed39a172aa9c5cda5a1b4853e8a8384714ac
MtoTheSchlee <michael_schleevoigt@icloud.com>
2024-01-30 18:53:00 +0100

    Initial Commit

---
+++ flutter_start_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flutter_start_1
+//
+//  Created by Michael Schleevoigt on 30.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flutter_start_1/flutter_start_1App.swift
@@ -0,0 +1,17 @@
+//
+//  flutter_start_1App.swift
+//  flutter_start_1
+//
+//  Created by Michael Schleevoigt on 30.01.24.
+//
+
+import SwiftUI
+
+@main
+struct flutter_start_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
